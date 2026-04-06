7a2647a20ac6778c48aea29ddbbed764ffa4b4d4
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-06 17:18:42 +0530

    terraform

--- provider.tf
+++ provider.tf
@@ -2,19 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.37.0"
+      version = "~> 6.0"
     }
   }
-
-  #configuring remote state in AWS S3
-/*   backend "s3" {
-    bucket         = "remote-state-pratyu-aws" # repalce with unique bucket name
-    key            = "vpc.tfstate" #key should be changed elase the same .tfstate will updtae which is wrong
-    region         = "us-east-1"
-    encrypt = true
-    use_lockfile = true  #this lock is mandatory to make sure .state file is not corupted when different users are using
-    
-  } */
 }
 
 # Configure the AWS Provider
